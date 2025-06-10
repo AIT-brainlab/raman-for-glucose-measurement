a63b4efc9d640aa5f5072c32367f336d283caf10
akraradets <akraradets@gmail.com>
2025-06-10 10:27:35 +0000

    add baseline but did not tested yet

--- raman/spectra/blood.py
+++ raman/spectra/blood.py
@@ -3,9 +3,9 @@ from ..sample import Sample
 from .function import load_raman_from_txt
 
 from pydantic import BaseModel, ConfigDict
-from typing import Self
+from typing import Self, Any
 from pymongo.errors import DuplicateKeyError
-
+from pymongo.collection import Collection
 
 import numpy as np
 from datetime import datetime
@@ -27,7 +27,7 @@ class Blood(BaseModel):
     power: float = 7.1
     slit: float = 7.1
 
-    glucose: float
+    glucose: int
     raman_shift: list[float]
     intensity: list[float]
 
@@ -87,7 +87,7 @@ class Blood(BaseModel):
                 elif key == "01":
                     continue
                 elif key == "name":
-                    item["glucose"] = float(value.split("-")[0]) # type: ignore
+                    item["glucose"] = int(value.split("-")[0]) # type: ignore
                     item["name"] = str(value.split("-")[1]) # type: ignore
                 else:
                     item[key] = value  # type: ignore
@@ -100,24 +100,44 @@ class Blood(BaseModel):
         return blood  # type: ignore
 
     @classmethod
-    def from_database(cls, name: str) -> Self:
+    def get_collection(cls) -> Collection[dict[str, Self]]:
+        """ 
+        Get the MongoDB collection for Blood spectra 
+        So you can perform queries directly on the collection
+        Returns:
+            collection: MongoDB collection for Blood spectra
+        """
+        return collection
+
+    @classmethod
+    def from_database_by_daterange(cls, start: datetime, end: datetime) -> list[Self]:
+        """
+        Load a raman spectrum from the database within a date range
+        Args:
+            start (datetime): Start date
+            end (datetime): End date
+        Returns:
+            list[Blood]: List of Blood objects
+        """
+        query = {"timestamp": {"$gte": start, "$lte": end}}
+        return cls.from_database(query=query)
+
+    @classmethod
+    def from_database(cls, query: dict[str, Any]) -> list[Self]:
         """
         Load a raman spectrum from the database
         Args:
-            name (str): Blood name
+            query (dict): Query to find the Blood object
         Returns:
             Blood object
         """
-        query = {"name": name}
-        item = collection.find_one(query)
-        if item is None:
-            raise ValueError(
-                f"Item with name {name} and timestamp not found"
-            )
-
-        blood = Blood(**item)
-        blood._id = item["_id"]
-        return blood  # type: ignore
+        cursor = collection.find(query).sort("timestamp", 1)
+        items: list[Self] = []
+        for item in cursor:
+            blood = Blood(**item)
+            blood._id = item["_id"]
+            items.append(blood) # type: ignore
+        return items  # type: ignore
 
     def to_sample(self, interpolate: bool = True, verbose: bool = True) -> Sample:
         """Convert the spectrum to a sample object
@@ -136,6 +156,7 @@ class Blood(BaseModel):
         sample.power = self.power
         sample.lens = self.lens
         sample.date = self.timestamp
+        sample.slit = self.slit
         return sample
 
     def save(self):
